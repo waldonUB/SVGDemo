8efff1190569aab6f754cefd5de98ba82f7670a2
waldonUB <852727515@qq.com>
2019-05-23 23:05:23 +0800

    重新创建svg项目

---
+++ src/main.js
@@ -0,0 +1,16 @@
+import Vue from "vue"
+import router from "./router"
+import App from "./App.vue"
+import "../src/css/reset.scss"
+import "../src/css/iconfont/iconfont.css"
+import "../src/css/design.scss"
+
+Vue.config.productionTip = false
+
+
+new Vue({
+    el: "#app",
+    router,
+    components: {App},
+    template: '<App></App>'
+})
---
+++ src/pages/designe/Top.vue
@@ -0,0 +1,27 @@
+<template>
+    <div id="headerNode" class="header">
+        <ul>
+            <li class="iconfont iconsave icon-header"></li>
+            <li class="iconfont iconcopy icon-header"></li>
+            <li class="iconfont iconcut icon-header"></li>
+            <li class="iconfont iconshrink1 icon-header"></li>
+            <li class="iconfont iconarrawsalt icon-header"></li>
+            <li class="iconfont iconshrink icon-header"></li>
+            <li class="iconfont iconmagnify icon-header"></li>
+            <li class="iconfont iconup-circle icon-header"></li>
+            <li class="iconfont icondown-circle icon-header"></li>
+            <li class="iconfont iconleft-circle icon-header"></li>
+            <li class="iconfont iconright-circle icon-header"></li>
+        </ul>
+    </div>
+</template>
+
+<script>
+    export default {
+        name: "Top"
+    }
+</script>
+
+<style scoped>
+
+</style>
